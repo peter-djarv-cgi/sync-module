68376303a00af66edb5d8ac996a6d715e51145a9
Peter Djärv <peter.jobb.cgi>
2025-02-24 09:37:38 +0100

    Add remote-path flag

--- mod.ts
+++ mod.ts
@@ -1,29 +1,35 @@
 import { LOG_COLORS, logMessage } from '@cgi/core-module';
 import { syncFile } from './utils/syncManager.ts';
 
-function parseFlags(): string {
+function parseFlags(): { filePath: string; remotePath?: string } {
   const args = Deno.args;
+  let filePath: string | undefined;
+  let remotePath: string | undefined;
 
   for (const arg of args) {
     const [key, value] = arg.split('=');
     if (key === 'file-path') {
-      return value;
+      filePath = value;
+    } else if (key === 'remote-path') {
+      remotePath = value;
     }
   }
 
-  // If the flag is not provided, log an error and exit
-  logMessage(
-    '%cERROR: Missing flag file-path. \nUsage: %cdeno run sync file-path=<path>',
-    LOG_COLORS.ERROR,
-    LOG_COLORS.COMMAND,
-  );
-  Deno.exit(1);
+  // Ensure filePath is provided
+  if (!filePath) {
+    logMessage(
+      '%cERROR: Missing required flag file-path. \nUsage: %cdeno run sync file-path=<path> [remote-path=<remote>]',
+      LOG_COLORS.ERROR,
+      LOG_COLORS.COMMAND,
+    );
+    Deno.exit(1);
+  }
+
+  return { filePath, remotePath };
 }
 
 // Executes the sync process
-const filePath = parseFlags();
-syncFile(filePath);
+const { filePath, remotePath } = parseFlags();
+syncFile(filePath, remotePath);
 
-export {
-  syncFile,
-};
+export { syncFile };
